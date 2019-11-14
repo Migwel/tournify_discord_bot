5dff5f6879e86b3a7a244c02f453730314884d8a
miguel <migonzalez1991@gmail.com>
2019-11-14 22:41:13 +0100

    Also handle errors when fetching participants (should probably be handled in a more generic manner)

--- src/main/java/dev/migwel/tournify/discordbot/listener/TournamentListener.java
+++ src/main/java/dev/migwel/tournify/discordbot/listener/TournamentListener.java
@@ -1,9 +1,11 @@
 package dev.migwel.tournify.discordbot.listener;
 
 import dev.migwel.tournify.communication.response.ParticipantsResponse;
+import dev.migwel.tournify.discordbot.ServerException;
 import dev.migwel.tournify.discordbot.messagewriter.MessageWriter;
 import dev.migwel.tournify.discordbot.messagewriter.MessageWriterFactory;
 import dev.migwel.tournify.discordbot.service.TournamentService;
+import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.TextChannel;
 import org.springframework.stereotype.Component;
 
@@ -25,8 +27,20 @@ public class TournamentListener {
     }
 
     public void action(TextChannel channel, String url) {
-        ParticipantsResponse participantsResponse = tournamentService.getParticipants(url);
+        ParticipantsResponse participantsResponse;
+        try {
+            participantsResponse = tournamentService.getParticipants(url);
+        } catch (ServerException e) {
+            writeMessage(channel, e.getMessage());
+            return;
+        }
         writeParticipants(channel, participantsResponse);
+    }
+
+    private void writeMessage(Channel channel, String message) {
+        TextChannel textChannel = channel.asTextChannel().orElseThrow(() -> new IllegalArgumentException("Wrong channel"));
+        MessageWriter messageWriter = messageWriterFactory.getMessageWriter(textChannel, message);
+        messageWriter.write();
     }
 
     private void writeParticipants(TextChannel channel, ParticipantsResponse participantsResponse) {
--- src/main/java/dev/migwel/tournify/discordbot/service/TournamentService.java
+++ src/main/java/dev/migwel/tournify/discordbot/service/TournamentService.java
@@ -2,10 +2,12 @@ package dev.migwel.tournify.discordbot.service;
 
 import dev.migwel.tournify.communication.request.TournamentRequest;
 import dev.migwel.tournify.communication.response.ParticipantsResponse;
+import dev.migwel.tournify.discordbot.ServerException;
 import dev.migwel.tournify.discordbot.properties.TournifyProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Nonnull;
@@ -24,10 +26,16 @@ public class TournamentService {
     }
 
     @Nonnull
-    public ParticipantsResponse getParticipants(String tournamentUrl) {
+    public ParticipantsResponse getParticipants(String tournamentUrl) throws ServerException {
         TournamentRequest participantsRequest = new TournamentRequest(tournamentUrl);
         log.info("Getting participants for "+ tournamentUrl);
-        return restTemplate.postForEntity(buildParticipantsUrl(), participantsRequest, ParticipantsResponse.class).getBody();
+        try {
+            return restTemplate.postForEntity(buildParticipantsUrl(), participantsRequest, ParticipantsResponse.class).getBody();
+        }
+        catch (RestClientException e) {
+            log.warn("Something went wrong while calling the server", e);
+            throw new ServerException("Could not fetch participants from "+ tournamentUrl, e);
+        }
     }
 
     private String buildParticipantsUrl() {
