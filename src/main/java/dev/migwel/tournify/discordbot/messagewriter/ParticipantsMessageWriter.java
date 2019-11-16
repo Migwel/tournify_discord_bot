6901fab78934a6356c68f0cc2e74b5a475e82630
miguel <migonzalez1991@gmail.com>
2019-11-16 16:25:57 +0100

    getParticipants give a Collection, not a List

--- src/main/java/dev/migwel/tournify/discordbot/messagewriter/ParticipantsMessageWriter.java
+++ src/main/java/dev/migwel/tournify/discordbot/messagewriter/ParticipantsMessageWriter.java
@@ -5,15 +5,15 @@ import org.javacord.api.entity.channel.TextChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class ParticipantsMessageWriter extends AbstractMessageWriter {
 
     private static final Logger log = LoggerFactory.getLogger(ParticipantsMessageWriter.class);
-    private List<Player> participants;
+    private Collection<Player> participants;
 
-    public ParticipantsMessageWriter(TextChannel channel, List<Player> participants) {
+    public ParticipantsMessageWriter(TextChannel channel, Collection<Player> participants) {
         super(channel);
         this.participants = participants;
     }
