3bce291172de1d524999fdbbc68745ac124e1d05
miguel <migonzalez1991@gmail.com>
2019-11-16 16:20:01 +0100

    Tournify was updated from String for participants to Players

--- src/main/java/dev/migwel/tournify/discordbot/messagewriter/ParticipantsMessageWriter.java
+++ src/main/java/dev/migwel/tournify/discordbot/messagewriter/ParticipantsMessageWriter.java
@@ -1,23 +1,25 @@
 package dev.migwel.tournify.discordbot.messagewriter;
 
+import dev.migwel.tournify.communication.commons.Player;
 import org.javacord.api.entity.channel.TextChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ParticipantsMessageWriter extends AbstractMessageWriter {
 
     private static final Logger log = LoggerFactory.getLogger(ParticipantsMessageWriter.class);
-    private List<String> participants;
+    private List<Player> participants;
 
-    public ParticipantsMessageWriter(TextChannel channel, List<String> participants) {
+    public ParticipantsMessageWriter(TextChannel channel, List<Player> participants) {
         super(channel);
         this.participants = participants;
     }
 
     protected String buildMessage() {
         String header = "List of participants: \n";
-        return header + String.join(", ", participants);
+        return header + participants.stream().map(Player::getDisplayUsername).collect(Collectors.joining(", ")) ;
     }
 }
