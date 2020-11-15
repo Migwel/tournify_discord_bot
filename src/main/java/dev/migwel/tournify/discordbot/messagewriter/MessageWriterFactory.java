69dae6826b0e9b8b4c19ab924fde025b7f774b72
migwel <migonzalez1991@gmail.com>
2020-11-15 15:15:07 +0100

    Use pattern matching

--- src/main/java/dev/migwel/tournify/discordbot/messagewriter/MessageWriterFactory.java
+++ src/main/java/dev/migwel/tournify/discordbot/messagewriter/MessageWriterFactory.java
@@ -10,24 +10,24 @@ import org.springframework.stereotype.Component;
 public class MessageWriterFactory {
 
     public MessageWriter getMessageWriter(TextChannel channel, Object messageContainer) {
-        if(messageContainer instanceof Update) {
-            return getUpdateMessageWriter(channel, (Update) messageContainer);
+        if(messageContainer instanceof Update update) {
+            return getUpdateMessageWriter(channel, update);
         }
 
-        if(messageContainer instanceof ParticipantsResponse) {
-            return new ParticipantsMessageWriter(channel, ((ParticipantsResponse)messageContainer).getParticipants());
+        if(messageContainer instanceof ParticipantsResponse participantsResponse) {
+            return new ParticipantsMessageWriter(channel, participantsResponse.getParticipants());
         }
 
-        if(messageContainer instanceof String) {
-            return new GenericMessageWriter(channel, (String) messageContainer);
+        if(messageContainer instanceof String genericMessage) {
+            return new GenericMessageWriter(channel, genericMessage);
         }
 
         throw new IllegalArgumentException("Wrong message container type: "+ messageContainer.getClass());
     }
 
     private MessageWriter getUpdateMessageWriter(TextChannel channel, Update update) {
-        if (update instanceof SetUpdate) {
-            return new SetUpdateMessageWriter(channel, ((SetUpdate)update).getSet());
+        if (update instanceof SetUpdate setUpdate) {
+            return new SetUpdateMessageWriter(channel, setUpdate.getSet());
         }
         return new GenericMessageWriter(channel, update.getDescription());
     }
